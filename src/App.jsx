0405b062c9d844408fb27363f0437dd2ae7de435
Panwar, Himanshu <himanshu.panwar@boeing.com>
2024-12-14 09:07:50 +0530

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
-import Product from './Product.jsx';
+import Product from './components/Product.jsx';
 import CartContextProvider from './store/shopping-cart-context.jsx';
 
 function App() {
